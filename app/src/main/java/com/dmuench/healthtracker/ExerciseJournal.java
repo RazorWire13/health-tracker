11e60dbb08b82f6c3423bb6fbac266d9df186eda
RazorWire13 <baneblade13@gmail.com>
2019-01-17 08:43:18 -0800

    exercise journal now working

--- app/src/main/java/com/dmuench/healthtracker/ExerciseJournal.java
+++ app/src/main/java/com/dmuench/healthtracker/ExerciseJournal.java
@@ -1,44 +1,67 @@
 package com.dmuench.healthtracker;
 
-import android.arch.persistence.room.Room;
-import android.support.v7.app.AppCompatActivity;
+import androidx.room.Room;
+import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
-import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.RecyclerView;
+import android.widget.EditText;
+import android.view.View;
+
+import java.util.Date;
+
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 // Code resourced from: http://www.vogella.com/tutorials/AndroidRecyclerView/article.html
 public class ExerciseJournal extends AppCompatActivity {
+    protected ExerciseDatabase exerciseDatabase;
 
     private RecyclerView mRecyclerView;
     private RecyclerView.Adapter mAdapter;
     private RecyclerView.LayoutManager mLayoutManager;
 
-    private ExerciseDatabase exerciseDatabase;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_exercise_journal);
 
+        mRecyclerView = (RecyclerView) findViewById(R.id.journal_recycler_view);
+        mRecyclerView.setHasFixedSize(true);
+
+        // Create a singleton database
         exerciseDatabase = Room.databaseBuilder(getApplicationContext(),
                 ExerciseDatabase.class, "exercise_db")
                 .allowMainThreadQueries()
+                .fallbackToDestructiveMigration()
                 .build();
 
-       if (exerciseDatabase.exerciseDao().getAll().isEmpty()) {
-            Exercises newExercise = new Exercises("Push-ups", "25", "Plank your body and push yourself up and down");
-            exerciseDatabase.exerciseDao().insertOneExercise(newExercise);
-        }
+        // use a linear layout manager
+        mLayoutManager = new LinearLayoutManager(this);
+        mRecyclerView.setLayoutManager(mLayoutManager);
 
-        mRecyclerView = (RecyclerView) findViewById(R.id.my_recycler_view);
-        mRecyclerView.setHasFixedSize(true);
+        // define an adapter
+        mAdapter = new MyAdapter(exerciseDatabase.exerciseDao().getAll());
+        mRecyclerView.setAdapter(mAdapter);
 
-            // use a linear layout manager
-            mLayoutManager = new LinearLayoutManager(this);
-            mRecyclerView.setLayoutManager(mLayoutManager);
-
-            // define an adapter
-            mAdapter = new MyAdapter(exerciseDatabase.exerciseDao().getAll());
-            mRecyclerView.setAdapter(mAdapter);
+        if (exerciseDatabase.exerciseDao().getAll().isEmpty()) {
+            Exercise newExercise = new Exercise("Push-ups", "25", "Plank your body and push yourself up and down", "DTG");
+            exerciseDatabase.exerciseDao().addExercise(newExercise);
         }
     }
+        public void addExerciseEntry (View v) {
+            EditText title = findViewById(R.id.editText4);
+            EditText description = findViewById(R.id.editText6);
+            EditText quantity = findViewById(R.id.editText5);
+            String timestamp = new Date().toString();
+
+            String titleEntry = title.getText().toString();
+            String descriptionEntry = description.getText().toString();
+            String quantityEntry = quantity.getText().toString();
+
+            Exercise newExercise = new Exercise(titleEntry, quantityEntry, descriptionEntry, timestamp);
+            exerciseDatabase.exerciseDao().addExercise(newExercise);
+
+            //got this from: https://stackoverflow.com/questions/3053761/reload-activity-in-android
+            finish();
+            startActivity(getIntent());
+        }
+}
--- app/src/main/java/com/dmuench/healthtracker/MainActivity.java
+++ app/src/main/java/com/dmuench/healthtracker/MainActivity.java
@@ -1,9 +1,9 @@
 package com.dmuench.healthtracker;
 
 import android.content.Intent;
-import android.support.v4.app.NotificationCompat;
-import android.support.v4.app.NotificationManagerCompat;
-import android.support.v7.app.AppCompatActivity;
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
+import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
